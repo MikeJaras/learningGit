b998436c4ebbbd450e75b452ab80a58deafb0531
MikeJaras <jaras@bahnhof.se>
2022-10-18 11:10:28 +0200

    new main

---
+++ main.py
@@ -0,0 +1,26 @@
+
+
+
+
+def main():
+    a = "ABC"
+    b = "123"
+
+    # for idx, t in enumerate(a):
+    #     print(t)
+    #     print(idx)
+
+    # for t in a, b:
+    #     print(t)
+
+    for p in range(len(a)):
+        print(a[p])
+        print(b[p])
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
